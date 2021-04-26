22f2820f1f8f638196bcd50026ec1558f4985abc
junkmaker <ksk_h12@yahoo.co.jp>
2021-04-26 23:43:00 +0900

    ファイル一覧作成

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
+import os, glob, hashlib
+import json
+
+targetDir = './data'
+fileDict = {}
+
+# ファイルの一覧
+files = glob.glob(targetDir + "/*")
+for f in files:
+    print(f)
+    fileDict[f] = glob.glob(f + "/*.txt")
+
+print(fileDict)
+print(json.dumps(fileDict))
+
 result = [[f'{num1 * num2:2}' for num2 in range(1, 10)] for num1 in range(1, 10)]
 
 for count, row in zip(range(1, 10), result):
